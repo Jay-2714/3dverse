28686e25f3691987d046d7a9023de674bf882543
jay2701 <jaysanjaymhatre2714@gmail.com>
2025-01-06 13:51:35 +0530

    temp

--- src/app/scripts/updatetext.py
+++ src/app/scripts/updatetext.py
@@ -1,18 +1,7 @@
 import bpy
 import sys
 
-def update_intro_text(new_text):
-    text_object = bpy.data.objects.get("TextObject")
-    if text_object and text_object.type == 'FONT':
-        text_object.data.body = new_text
-        print(f"Updated text to: {new_text}")
-    else:
-        print("Text object not found!")
-
-if __name__ == "__main__":
-    args = sys.argv
-    if len(args) > 1:
-        new_text = args[1]
-        update_intro_text(new_text)
-    else:
-        print("No text provided.")
+text_obj = bpy.data.objects.get('Text')
+if text_obj:
+    text_obj.data.body = ''
+    
